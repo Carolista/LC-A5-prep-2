2f3d3bb5c9473cbc22c900fbf4e365a0ef9d462a
Carolista <cjones.renaissancewoman@gmail.com>
2020-10-23 22:59:03 -0500

    added files and basic HTML elements

---
+++ scripts.js
@@ -0,0 +1,19 @@
+/*** PHOTOLOCATION ***/
+
+/*
+    Assignment 5 Prep 2 - Integrating the DOM with forms and fetching JSON
+*/
+
+// Event listener for page load
+window.addEventListener("load", function() {
+    console.log('Page loaded.');
+    init();
+});
+
+// DOM code for page elements
+function init() {
+
+
+
+
+}
